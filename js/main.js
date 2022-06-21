064b49f1127c410ea22a640dd748c69876ad254a
Mauricio Oliveira <mauriciomholiveira@icloud.com>
2022-06-21 00:09:04 -0300

    finalizado testimonials

--- js/main.js
+++ js/main.js
@@ -89,3 +89,21 @@ modalCloses.forEach((modalClose) => {
     })
 })
 /* =============== SWIPER TESTIMONIAL ======================= */
+let swiper = new Swiper(".testimonials_container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
